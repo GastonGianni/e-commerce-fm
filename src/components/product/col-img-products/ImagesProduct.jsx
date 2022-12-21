8802fe9e4cbb847a7ff2519c3c69a7b8f825bf78
giann <gianni.gaston@hotmail.com>
2022-12-21 18:05:25 -0300

    modal, falta terminar

--- src/components/product/col-img-products/ImagesProduct.jsx
+++ src/components/product/col-img-products/ImagesProduct.jsx
@@ -3,17 +3,35 @@ import { PrevIcon } from "@/components/icons/PrevIcon";
 import { NextIcon } from "@/components/icons/NextIcon";
 
 import { useState } from "react";
+import CloseIcon from "../../icons/CloseIcon";
 
-const ImagesProduct = ({ IMG_ARRAY = [], IMG_SMALL_ARRAY = [], ...props }) => {
+const ImagesProduct = ({ IMG_ARRAY = [], IMG_SMALL_ARRAY = [], isModalOpen = false, handleCloseModal = null, handleOpenModal = null, ...props }) => {
+  const [imgIndex, setImgIndex] = useState(0);
+
+  const handleClickNext = (e) => {
+    imgIndex === IMG_ARRAY.length - 1 ? setImgIndex(0) : setImgIndex(imgIndex + 1);
+  };
+  const handleClickPrev = (e) => {
+    imgIndex === 0 ? setImgIndex(IMG_ARRAY.length - 1) : setImgIndex(imgIndex - 1);
+  };
+
+  const changeImg = (e) => {
+    setImgIndex(e.target.id);
+  };
   return (
-    <section className="mx-auto grid md:max-w-[524px] md:grid-cols-4 md:gap-4">
-      <div className=" relative col-span-4 ">
-        <img src={IMG_ARRAY[props.imgIndex]} alt="" className="aspect-[16/14] w-full md:mx-auto md:aspect-[16/14] md:rounded-lg " />
+    <section {...props}>
+      {isModalOpen && (
+        <button onClick={handleCloseModal} className="col-span-4 flex items-center justify-end">
+          <CloseIcon />
+        </button>
+      )}
+      <div onClick={handleOpenModal} className="relative col-span-4 md:cursor-zoom-in">
+        <img src={IMG_ARRAY[imgIndex]} alt="" className="aspect-[16/14] w-full md:mx-auto md:aspect-[16/14] md:rounded-lg " />
         <div className="absolute top-1/2 flex w-full -translate-y-1/2 justify-between px-4">
-          <button onClick={props.handleClickPrev} className=" flex h-9 w-9 items-center justify-center rounded-full bg-white md:hidden">
+          <button onClick={handleClickPrev} className=" flex h-9 w-9 items-center justify-center rounded-full bg-white md:hidden">
             <PrevIcon />
           </button>
-          <button onClick={props.handleClickNext} className=" flex h-9 w-9 items-center justify-center rounded-full bg-white md:hidden">
+          <button onClick={handleClickNext} className=" flex h-9 w-9 items-center justify-center rounded-full bg-white md:hidden">
             <NextIcon />
           </button>
         </div>
@@ -23,10 +41,10 @@ const ImagesProduct = ({ IMG_ARRAY = [], IMG_SMALL_ARRAY = [], ...props }) => {
         <img
           key={smallImg.id}
           id={smallImg.id}
-          onClick={props.changeImg}
+          onClick={changeImg}
           src={smallImg.url}
           alt=""
-          className="hidden rounded-lg transition-all duration-300 hover:scale-105 md:block"
+          className="hidden cursor-pointer rounded-lg transition-all duration-300 hover:scale-105 md:block"
         />
       ))}
     </section>
--- src/components/product/col-img-products/MainImagesProduct.jsx
+++ src/components/product/col-img-products/MainImagesProduct.jsx
@@ -2,27 +2,33 @@ import ImagesProduct from "@/components/product/col-img-products/ImagesProduct";
 import { useState } from "react";
 
 export default ({ IMG_ARRAY, IMG_SMALL_ARRAY }) => {
-  const [imgIndex, setImgIndex] = useState(0);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const handleClickNext = (e) => {
-    imgIndex === IMG_ARRAY.length - 1 ? setImgIndex(0) : setImgIndex(imgIndex + 1);
-  };
-  const handleClickPrev = (e) => {
-    imgIndex === 0 ? setImgIndex(IMG_ARRAY.length - 1) : setImgIndex(imgIndex - 1);
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
   };
 
-  const changeImg = (e) => {
-    setImgIndex(e.target.id);
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
   };
 
   return (
-    <ImagesProduct
-      IMG_ARRAY={IMG_ARRAY}
-      IMG_SMALL_ARRAY={IMG_SMALL_ARRAY}
-      handleClickNext={handleClickNext}
-      handleClickPrev={handleClickPrev}
-      changeImg={changeImg}
-      imgIndex={imgIndex}
-    />
+    <>
+      <ImagesProduct
+        IMG_ARRAY={IMG_ARRAY}
+        IMG_SMALL_ARRAY={IMG_SMALL_ARRAY}
+        handleOpenModal={handleOpenModal}
+        className="mx-auto grid md:max-w-[524px] md:grid-cols-4 md:gap-4"
+      />
+      {isModalOpen && (
+        <ImagesProduct
+          IMG_ARRAY={IMG_ARRAY}
+          IMG_SMALL_ARRAY={IMG_SMALL_ARRAY}
+          isModalOpen={isModalOpen}
+          handleCloseModal={handleCloseModal}
+          className="mx-auto hidden md:absolute md:top-1/2 md:left-1/2 md:z-20 md:grid md:min-w-[600px] md:max-w-[660px] md:-translate-y-1/2 md:-translate-x-1/2 md:grid-cols-4 md:gap-4"
+        />
+      )}
+    </>
   );
 };

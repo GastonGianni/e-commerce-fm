bc65e49916732ae4f1fdc633538c42c456699feb
giann <gianni.gaston@hotmail.com>
2022-12-26 20:26:19 -0300

    Terminado cart mobile, interaccion con carritoBtn, llevar logica a otro componente!!

--- src/components/header/MainHeader.jsx
+++ src/components/header/MainHeader.jsx
@@ -10,6 +10,7 @@ import DeleteIcon from "@/components/icons/DeleteIcon";
 
 const MainHeader = () => {
   const [isOpenMenu, setIsOpenMenu] = useState(false);
+  const [isOpenCart, setIsOpenCart] = useState(false);
 
   const MOBILE_MENU_OPEN_CLASS = "absolute top-0 left-0 z-10 flex h-full w-4/6 flex-col gap-y-5 bg-white p-8 font-bold";
 
@@ -19,6 +20,8 @@ const MainHeader = () => {
   const handleCloseMenu = () => {
     setIsOpenMenu(false);
   };
+
+  const handleOpenCart = () => setIsOpenCart(!isOpenCart);
 
   return (
     <>
@@ -40,29 +43,36 @@ const MainHeader = () => {
         </nav>
         <div className="flex gap-4">
           <button>
-            <CartIcon />
+            <CartIcon onClick={handleOpenCart} />
           </button>
           <img src={imagenAvatar} alt="" className="w-10" />
 
-          <section className="absolute top-[125%] left-0 z-10 hidden w-full bg-red-300">
-            <div className="mx-4 rounded-lg bg-white">
-              <h4 className="p-5 font-bold">Cart</h4>
-              <hr />
-              <div className="grid grid-cols-[1fr_4fr_1fr] gap-4 p-5">
-                <img className="rounded-lg" src={imgCartSmall} alt="" />
-                <div className="">
-                  <h6>Fall Limited Edition Sneakers</h6>
-                  <div>
-                    <span>$125.00 x 3</span>
-                    <span className="font-bold">$375.00</span>
+          {isOpenCart && (
+            <section className="absolute top-[125%] left-0 z-10 w-full md:hidden">
+              <div className="mx-4 rounded-lg bg-white pb-7 shadow-lg">
+                <h4 className="p-5 font-bold">Cart</h4>
+                <hr />
+                <div className="grid grid-cols-[1fr_4fr_1fr] gap-4 p-5">
+                  <img className="min-w-[55px] rounded-lg" src={imgCartSmall} alt="" />
+                  <div className="flex-col items-center justify-center">
+                    <h6 className="text-sm text-dark-grayish">Fall Limited Edition Sneakers</h6>
+                    <div className="flex gap-3">
+                      <span className="text-sm text-dark-grayish">$125.00 x 3</span>
+                      <span className="text-sm font-bold">$375.00</span>
+                    </div>
                   </div>
+                  <button>
+                    <DeleteIcon />
+                  </button>
                 </div>
-                <button>
-                  <DeleteIcon />
-                </button>
+                <div className="mx-auto flex w-5/6 items-center justify-center">
+                  <button className="w-full rounded-lg bg-orange-personal px-2 py-3 font-bold text-white shadow-lg hover:bg-orange-400">
+                    Checkout
+                  </button>
+                </div>
               </div>
-            </div>
-          </section>
+            </section>
+          )}
         </div>
       </header>
       <hr className="container mx-auto hidden md:block" />

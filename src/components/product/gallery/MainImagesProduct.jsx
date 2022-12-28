d222b9af0d6420729d8a2745f639e82aa4dcee17
giann <gianni.gaston@hotmail.com>
2022-12-28 14:46:59 -0300

    index del modal

--- src/components/product/gallery/MainImagesProduct.jsx
+++ src/components/product/gallery/MainImagesProduct.jsx
@@ -24,7 +24,7 @@ export default ({ IMG_ARRAY, IMG_SMALL_ARRAY }) => {
             IMG_SMALL_ARRAY={IMG_SMALL_ARRAY}
             isModalOpen={isModalOpen}
             handleCloseModal={handleCloseModal}
-            className="mx-auto hidden md:absolute md:top-1/2 md:left-1/2 md:z-10 md:grid md:min-w-[580px] md:max-w-[620px] md:-translate-y-1/2 md:-translate-x-1/2 md:grid-cols-4 md:gap-4"
+            className="mx-auto hidden md:absolute md:top-1/2 md:left-1/2 md:z-20 md:grid md:min-w-[580px] md:max-w-[620px] md:-translate-y-1/2 md:-translate-x-1/2 md:grid-cols-4 md:gap-4"
           />
 
           <span onClick={handleCloseModal} className="md:fixed md:top-0 md:left-0 md:h-full md:w-full md:backdrop-blur-md"></span>

41de9c1151c35b50e6ebd333c8de93f92d372f58
Gaston_Gianni <gianni.gaston@hotmail.com>
2023-01-19 16:09:13 -0300

    Imagen carrusel

--- src/components/header/MainHeader.jsx
+++ src/components/header/MainHeader.jsx
@@ -8,9 +8,10 @@ import CartProductHeader from "@/components/header/CartProductHeader";
 import { useCartContext } from "../../context/CartContext";
 
 const MainHeader = () => {
-  const { isOpenCart, isOpenMenu, toggleCart, toggleMenu, finalProductQuant } = useCartContext();
+  const { isOpenCart, isOpenMenu, toggleCart, toggleMenu, finalProductQuant } =
+    useCartContext();
 
-  const links = ["Home", "Collection", "About", "Contact"];
+  const links = ["Home", "Collection", "About", "Contact", "Product"];
 
   const handleToggleMenu = () => {
     toggleMenu();
@@ -24,13 +25,25 @@ const MainHeader = () => {
     "fixed top-0 left-0 z-20 flex h-full w-4/6 flex-col gap-y-5 p-8 font-bold bg-white border-r-2 border-slate-300 md:font-normal md:border-0";
   return (
     <>
-      {isOpenMenu && <span className={`fixed z-10 h-full w-full bg-black opacity-75 md:hidden`}></span>}
+      {isOpenMenu && (
+        <span
+          className={`fixed z-10 h-full w-full bg-black opacity-75 md:hidden`}
+        ></span>
+      )}
       <header className="container relative mx-auto flex items-center gap-8 p-6 md:p-0">
         <button className="md:hidden" onClick={handleToggleMenu}>
           <MenuIcon />
         </button>
-        <img src={logoSneakers} alt="Logo Sneakers" className="mr-auto mb-1 h-5 md:mr-0" />
-        <nav className={`md:static md:mr-auto md:flex md:flex-row md:gap-4 md:p-0 ${isOpenMenu ? MOBILE_MENU_OPEN_CLASS : "hidden"}`}>
+        <img
+          src={logoSneakers}
+          alt="Logo Sneakers"
+          className="mr-auto mb-1 h-5 md:mr-0"
+        />
+        <nav
+          className={`md:static md:mr-auto md:flex md:flex-row md:gap-4 md:p-0 ${
+            isOpenMenu ? MOBILE_MENU_OPEN_CLASS : "hidden"
+          }`}
+        >
           <button className="mb-12 w-5 md:hidden" onClick={handleToggleMenu}>
             <CloseIcon />
           </button>
@@ -43,7 +56,9 @@ const MainHeader = () => {
             <CartIcon />
             {finalProductQuant > 0 && (
               <span className="absolute top-1 h-4 w-5 rounded-xl bg-orange-400">
-                <p className="text-center text-xs font-bold text-white">{finalProductQuant}</p>
+                <p className="text-center text-xs font-bold text-white">
+                  {finalProductQuant}
+                </p>
               </span>
             )}
           </button>
@@ -53,7 +68,12 @@ const MainHeader = () => {
       </header>
       <hr className="container mx-auto hidden md:block" />
 
-      {isOpenCart && <span onClick={toggleCart} className="fixed top-0 left-0 z-10 h-full w-full bg-slate-400 bg-opacity-5"></span>}
+      {isOpenCart && (
+        <span
+          onClick={toggleCart}
+          className="fixed top-0 left-0 z-10 h-full w-full bg-slate-400 bg-opacity-5"
+        ></span>
+      )}
     </>
   );
 };

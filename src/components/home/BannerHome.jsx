35d96fa143d517351502e2a940682ef7e7a5deeb
Gaston_Gianni <gianni.gaston@hotmail.com>
2023-01-21 15:19:27 -0300

    banner home

--- src/components/home/BannerHome.jsx
+++ src/components/home/BannerHome.jsx
@@ -3,7 +3,9 @@ import React from "react";
 export default function BannerHome() {
   return (
     <div>
-      <div className="h-[320px] max-h-[320px] min-h-[120px] w-full border bg-pink-300 "></div>
+      <div className="h-[320px] max-h-[320px] min-h-[120px] w-full border bg-pink-300 ">
+        <h2></h2>
+      </div>
     </div>
   );
 }
